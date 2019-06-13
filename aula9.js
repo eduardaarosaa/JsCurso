3028d480bed1a5989273d7fd43f588de95c4e741
eduardaarosaa <eduardacirina@gmail.com>
2019-06-13 15:56:17 -0300

    aula9

---
+++ aula9.js
@@ -0,0 +1,9 @@
+var idade = 18;
+
+if(idade >= 18 && idade <= 65){
+    console.log("Obrigatório votar")
+}else if (idade >= 16 && idade <= 17){
+    console.log("Já vota mas não é obrigatório")
+}else{
+    console.log("Menor de idade");
+}
